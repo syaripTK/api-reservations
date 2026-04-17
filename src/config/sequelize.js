5704c5b7e380390905ac6eb913aaaeb97db92537
Ahmad Syangkan Syarip <ahmadssyarif27@gmail.com>
2026-04-17 08:47:59 +0700

    fix

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
 const config = require("./database.js").development;
 
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
+
 const sequelize = new Sequelize(
   config.database,
   config.username,
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app.js");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.DB_PORT;
 
 const server = app.listen(PORT, "0.0.0.0", () => {
   console.info(`Server stabil berjalan di port ${PORT}`);
